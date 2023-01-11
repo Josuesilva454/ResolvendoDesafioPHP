6c606233057df376dda52d59c6b15f4029b3f814
Josué Silva Rosa <84036932+Josuesilva454@users.noreply.github.com>
2023-01-11 17:52:48 -0300

    Update raciocinio.php

--- desafioPhp/raciocinio.php
+++ desafioPhp/raciocinio.php
@@ -1,14 +1,77 @@
+<?php
 //Leia um valor inteiro correspondente à idade de uma pessoa em dias e informe-a em anos, meses e dias
 
 //Observação: apenas para facilitar o cálculo, considere todo ano com 365 dias e todo mês com 30 dias. Nos casos de teste nunca haverá uma 
 
 //situação que permita 12 meses e alguns dias, como 360, 363 e 364.s.
 
-$idade = readline('Informe a sua idade: ');
-$anos = readline('Informe a sua anos: ');
-$meses = readline('Informe a sua meses: ');
-$dias = readline('Informe a sua dias: ');
-$DIAS_NO_ANO = 365;
-$DIAS_NO_MES = 30;
-$dias += ($anos * $DIAS_NO_ANO) + ($meses * $DIAS_NO_MES);
-echo ("%d ano(s)\n%d mes(es)\n%d dia(s)\n" . $dias);
+
+$N = readline(' Informe o numero: ');
+
+$ANOS=$N/365;
+
+$MESES=($N%365)/30;
+
+$DIAS=($N%365)%30;
+
+echo (" ano(s)\n ".$ANOS ." mes(es)\n ".$MESES ." dia(s)\n ".$DIAS);
+
+
+function mdc($a, $b)
+{
+if($b == 0)
+    return $a;
+else
+return mdc($b, $a%$b);
+    
+}
+
+function mdf()
+{
+    //cria o objeto bufferedreader
+}
+    //criação de variaveis
+    $c = readline(' Informe o numero: ');
+     
+ 
+    
+    //while para executar os c casos
+    while(  $c-- > 0){
+        //lê as frações e a operação
+    $N1 = readline(' Informe o numero: ');
+    $D1 = readline(' Informe o numero: ');
+    $op = readline(' Informe o numero: ');
+    $n2 = readline(' Informe o numero: ');
+    $d2 = readline(' Informe o numero: ');
+        //realiza a operação
+        if($op == '+'){
+            $num = $N1 * $d2 + $n2 * $D1;
+            $den = $D1 * $d2;
+            $simp = mdc($num, $den); //calcula o mdc para simpificar
+            if($simp < 0)
+                $simp *= -1;               
+            echo($num + "/" . $den + " = " . ($num / $simp) + "/" . ($den / $simp));
+        }else if($op == '-'){
+            $num = $N1  * $d2 - $n2 * $D1;
+            $den = $D1 * $d2;
+            $simp = mdc($num, $den); //calcula o mdc para simpificar
+            if($simp < 0)
+                $simp *= -1;
+            echo($num + "/" . $den . " = " + ($num / $simp) + "/" . ($den / $simp));
+        }else if($op == '*'){
+            $num = $N1 * $n2;
+            $den = $D1 * $d2;
+            $simp = mdc($num, $den); //calcula o mdc para simpificar
+            if($simp < 0)
+                $simp *= -1;
+           echo($num + "/" . $den + " = " . ($num / $simp) . "/" . ($den / $simp));
+        }else if($op == '/'){
+            $num = $N1 * $d2;
+            $den = $n2 * $D1;
+            $simp = mdc($num, $den); //calcula o mdc para simpificar
+            if($simp < 0)
+                $simp *= -1;
+            echo($num + "/" . $den . " = " . ($num / $simp) . "/" . ($den / $simp));
+        }
+    }
+?>
